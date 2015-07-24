7fc5ea6f0a1df05fbf66436e5746caf6937b7df0
tbeeler <tbeeler@apple.com>
2015-07-24 07:42:55 -0700

    increased version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'timothybeeler@me.com'
 license          'all_rights'
 description      'Installs/Configures workstation_home'
 long_description 'Installs/Configures workstation_home'
-version          '0.2.1'
+version          '0.2.2'
 
