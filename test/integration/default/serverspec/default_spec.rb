0ec3f2c2c9a1de5ab5958b3f3440cf0debdf5df2
tbeeler <tbeeler@apple.com>
2015-07-23 21:50:23 -0700

    first test case

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,8 +5,15 @@ describe 'workstation_home::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  # test motd_home recipe
+  describe file('/etc/motd') do
+    it { should exist }
+    it { should contain 'Welcome home!'}
   end
 
+  #test sshd_config_home recipe
+  describe file ('/etc/sshd_config')
+    it { should exist}
+    it { should contain '#sshd_config_home'}
+  end
 end
