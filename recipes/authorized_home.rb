11b90591d9fe58b717a96934e3af767496340475
tbeeler <tbeeler@apple.com>
2015-07-23 21:49:52 -0700

    placeholder for recipe not yet written

---
+++ recipes/authorized_home.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: workstation_home
+# Recipe:: authorized
+#
+# Copyright (c) 2015 Tim Beeler, All Rights Reserved.
---
+++ recipes/homebrew_home.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: workstation_home
+# Recipe:: homebrew_home
+#
+# Copyright (c) 2015 Tim Beeler, All Rights Reserved.
