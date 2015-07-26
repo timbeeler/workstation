ff0ba5ee3f04aab57cd2a3554fed10fe67d9015a
tbeeler <tbeeler@apple.com>
2015-07-25 20:32:20 -0700

    created home-brew recipe

--- recipes/homebrew_home.rb
+++ recipes/homebrew_home.rb
@@ -3,3 +3,25 @@
 # Recipe:: homebrew_home
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
+
+homebrew_tap 'caskroom/cask'
+
+package "brew-cask" do
+  action :install
+  end
+
+homebrew_cask 'google-chrome'
+homebrew_cask 'dropbox'
+homebrew_cask 'vagrant'
+homebrew_cask 'sublime-text'
+homebrew_cask 'github'
+homebrew_cask 'divvy'
+homebrew_cask 'iterm2'
+homebrew_cask 'base'
+homebrew_cask 'gpgtools'
+
+homebrew_package 'pinentry'
+homebrew_package 'ddrescue'
+homebrew_package 'chkrootkit'
+homebrew_package 'gpg-agent'
+homebrew_package 'nmap'
