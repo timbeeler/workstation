b02a318a6b281be132acd379ad4252da66100e2d
tbeeler <tbeeler@apple.com>
2015-07-25 20:32:01 -0700

    included homebrew and sw_update in run list

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,5 @@
 
 include_recipe "workstation_home::motd_home"
 include_recipe "workstation_home::sshd_config_home"
+include_recipe "workstation_home::homebrew_home"
+include_recipe "workstation_home::sw_update"
