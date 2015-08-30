c398c3384d32d11c30962a0e91615993411a56d9
tbeeler <tbeeler@apple.com>
2015-08-30 08:04:54 -0700

    Stripping MOTD info

--- recipes/motd_home.rb
+++ recipes/motd_home.rb
@@ -4,11 +4,7 @@
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
 file "/etc/motd" do
-  content "Welcome home! Host details:
-  HOSTNAME: #{node["hostname"]}
-  Memory: #{node["memory"]["total"]}
-  Uptime: #{node["uptime"]}
-"
+  content "Welcome to HOSTNAME: #{node["hostname"]}"
   mode "0644"
   action :create
 end
