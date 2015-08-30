67764aacdbab9bd00c7d4c52f054582c9d79f5f5
tbeeler <tbeeler@apple.com>
2015-08-30 08:05:08 -0700

    updating version number

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'timothybeeler@me.com'
 license          'all_rights'
 description      'Installs/Configures workstation_home'
 long_description 'Installs/Configures workstation_home'
-version          '0.4.0'
+version          '0.4.6'
 
 depends 'homebrew', '~> 1.13.0'
