ef5464b1bbfceb22aebfe30133676e57e352cbf9
Tim Beeler <timothybeer@me.com>
2015-08-30 19:40:00 -0700

    Removed MOTD and added ZSHRC

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
 
-include_recipe "workstation_home::motd_home"
 include_recipe "workstation_home::sshd_config_home"
 include_recipe "workstation_home::homebrew_home"
-include_recipe "workstation_home::sw_update"
+#include_recipe "workstation_home::sw_update"
+include_recipe "workstation_home::zshrc_home"
