1618d8c61e766e8711362879f421b54de68686fd
Tim Beeler <timothybeer@me.com>
2015-08-30 21:48:06 -0700

    adjusted chef-client

--- recipes/chef_client_cron_home.rb
+++ recipes/chef_client_cron_home.rb
@@ -1,11 +1,11 @@
 #
 # Cookbook Name:: workstation_home
-# Recipe:: homebrew_home
+# Recipe:: chef_client_cron_home
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
 
 cron 'chef-client' do
-  hour '*/1'
+  hour '*/59'
   command 'chef-client'
   user 'root'
 end
