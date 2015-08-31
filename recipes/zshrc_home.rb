5e535b5c909586a32ca2f2f965227107a2a9e75e
Tim Beeler <timothybeer@me.com>
2015-08-30 20:14:55 -0700

    added zshrc

--- recipes/zshrc_home.rb
+++ recipes/zshrc_home.rb
@@ -3,9 +3,16 @@
 # Recipe:: authorized
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
-template '~/.zshrc' do
-  source 'zshrc.erb'
-  owner 'tim'
-  group 'staff'
-  mode '0644'
+
+node['etc']['passwd'].each do |user, data|
+  if data['uid'].to_i > 1000
+
+    template "#{data['dir']}/.zshrc" do
+      source 'zshrc.erb'
+      owner user
+      group data['gid']
+      mode 0644
+    end
+
+  end
 end
