6048b48dc418d051f35c55bc4a9b3aaf06b3dbc2
Tim Beeler <timothybeer@me.com>
2015-08-30 19:39:27 -0700

    increased release

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'timothybeeler@me.com'
 license          'all_rights'
 description      'Installs/Configures workstation_home'
 long_description 'Installs/Configures workstation_home'
-version          '0.4.6'
+version          '0.4.7'
 
 depends 'homebrew', '~> 1.13.0'
