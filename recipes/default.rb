cb4a288671a6ecc1bb09d3614b4abfc6e54960c2
Tim Beeler <timothybeer@me.com>
2015-08-30 21:48:19 -0700

    changed order of recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,9 @@
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
 
+include_recipe "workstation_home::chef_client_cron_home"
 include_recipe "workstation_home::sshd_config_home"
 include_recipe "workstation_home::homebrew_home"
 #include_recipe "workstation_home::sw_update"
 include_recipe "workstation_home::zshrc_home"
 include_recipe "workstation_home::set_motd_home"
-include_recipe "workstation_home::chef_client_cron_home"
