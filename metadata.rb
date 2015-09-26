23efdc421edb88e19295283ab75b87cdbb71d0e7
tbeeler <tbeeler@apple.com>
2015-09-26 14:44:09 -0700

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'timothybeeler@me.com'
 license          'all_rights'
 description      'Installs/Configures workstation_home'
 long_description 'Installs/Configures workstation_home'
-version          '0.6.1'
+version          '0.6.2'
 
 depends 'homebrew', '~> 1.13.0'
