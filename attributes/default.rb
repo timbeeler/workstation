598038685139de3778379e56aa2c8225fbee7057
tbeeler <tbeeler@apple.com>
2015-09-26 10:38:54 -0700

    added chef-client attribute

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default[:chef_client][:bin]           = "/usr/bin/chef-client"
+default[:chef_client][:log]           = "/var/log/chef/client.log"
+default[:chef_client][:config]        = "/etc/chef/client.rb"
+default[:chef_client][:interval]      = 1800
+default[:chef_client][:splay]         = 600
+default[:chef_client][:organization]  = "home-chef-tbeeler"
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,8 @@
 #
 # Copyright (c) 2015 Tim Beeler, All Rights Reserved.
 
-include_recipe "workstation_home::chef_client_cron_home"
 include_recipe "workstation_home::sshd_config_home"
 include_recipe "workstation_home::homebrew_home"
-#include_recipe "workstation_home::sw_update"
 include_recipe "workstation_home::zshrc_home"
-#include_recipe "workstation_home::set_motd_home"
 include_recipe "workstation_home::motd_home"
+include_recipe "workstation_home::chef_client"
